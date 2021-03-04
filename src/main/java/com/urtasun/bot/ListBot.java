1b2cb47171e9cd1d386a9ddb797cb67e578f4153
urtasunlautaro <urtasunlautaro@gmail.com>
2021-03-04 19:36:15 -0300

    llevé token y username a properties fuera de git.

--- src/main/java/com/urtasun/bot/ListBot.java
+++ src/main/java/com/urtasun/bot/ListBot.java
@@ -2,6 +2,7 @@ package com.urtasun.bot;
 
 import com.urtasun.handler.Handler;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
@@ -12,6 +13,10 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 @RequiredArgsConstructor
 public class ListBot extends TelegramLongPollingBot {
 
+  @Value("${com.urtasun.bot-username}")
+  private String username;
+  @Value("${com.urtasun.bot-token}")
+  private String token;
   private final Handler handler;
 
   public String getBotUsername() {
